6c793da03b628c584e8df063f891606b00e77f71
HumanUser <is.homutov@gmail.com>
2025-11-07 18:53:59 +0100

    fix local dev server routing

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
         client: false,
         webSocketServer: false,
         proxy: 'http://127.0.0.1:8000',
+        historyApiFallback: true,
     },
     configureWebpack: {
         plugins: [
